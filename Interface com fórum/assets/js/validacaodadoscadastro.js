02215ec597ffb6c2659c0623378df7e77594a667
giovaniqwer <giovanijunior7@yahoo.com.br>
2016-06-02 11:15:35 -0300

    Update validacaodadoscadastro.js
    ALTERAÇÃO DO CÓDIGO (NOVA FUNCIONALIDADE)-GIOVANI

--- Interface com fórum/assets/js/validacaodadoscadastro.js
+++ Interface com fórum/assets/js/validacaodadoscadastro.js
@@ -2,13 +2,23 @@
 //Javascript para fazer validação dos dados digitados durante o cadastro
 
 function verificadadoscadastro () {
-	var senha = document.getElementById("senha").value;
-	verificasenha(senha);
+	//Chamando as verificações
+	verificaSenha();
+	limpaCampos();
 }
 
-//Verificar dados relacionados a senha
-function verificasenha (senha) {
-	if (senha.length < 6) {
+//Verificar dados relacionados a senha (Se há 6 caracteres/ Se as senhas conferem)
+function verificaSenha (senha, confSenha) {
+	if (document.getElementById("senha").value.length < 6) {
 		alert("A senha deve ter pelo menos 6 caracteres");
+	}else if (document.getElementById("senha").value != document.getElementById("cofirmacao_senhacadastro").value) {
+		alert("As senhas digitadas não conferem!");
 	}
 }
+
+//Limpa todos os campos
+function limpaCampos() {
+	document.getElementById("senha").value = "";
+	document.getElementById("cofirmacao_senhacadastro").value;
+}
+
