f3404cbdc44ade873614279a436696814d44388b
giovaniqwer <giovanijunior7@yahoo.com.br>
2016-08-16 16:10:17 -0300

    Update add-cadastro.php
    Validar se email já está cadastrado

--- site/add-cadastro.php
+++ site/add-cadastro.php
@@ -1,6 +1,25 @@
 <?php
  require_once 'init.php';
  include_once 'cadastro-class.php';
+
+ //INICIO ALTERAÇÃO GIOVANI
+//CONFERE SE O E-MAIL JÁ ESTÁ CADASTRADO NO BANCO DE DADOS ANTES DE ENVIAR
+
+$email = $_POST['email'];
+$pdo = new PDO("mysql:host=localhost; dbname=LPTI", "root", "root");
+$stm = $pdo->prepare("SELECT * FROM Usuario where email='$email' ");
+$stm->execute();
+$nlinhas= $stm->rowCount();
+if ($nlinhas!=0){
+//Caso o dado já esteja cadastrado
+echo "<script>alert('Este email já está sendo usado!');location.href='cadastro.php'</script>";
+//echo "<script>javascript:history.go(-2);</script>";
+}
+//Caso NÃO esteja cadastrado
+else{
+  //FIM ALTERAÇÃO GIOVANI
+
+
  // pega os dados do formulário
  $nome = isset($_POST['first_name']) ? $_POST['first_name'] : null;
  $sobrenome = isset($_POST['last_name']) ? $_POST['last_name'] : null;
@@ -8,6 +27,7 @@
  $email = isset($_POST['email']) ? $_POST['email'] : null;
  $matricula= isset($_POST['matricula']) ? $_POST['matricula'] : null;
  $tipo = 2;
+
 
 
  $usuario = new Usuario($nome,$sobrenome,$senha, $email, $matricula, $tipo);
@@ -24,9 +44,10 @@
  $stmt->bindParam(':tipo', $usuario->getTipo());
 
  if($stmt->execute()){
-    header ('Location: login.php');
+   echo "<script>alert('Cadastro realizado com sucesso');location.href='login.php'</script>";
  }else{
     echo "Erro ao cadastrar!!";
     print_r($stmt->errorInfo());
- }
+ }}
 ?>
+
