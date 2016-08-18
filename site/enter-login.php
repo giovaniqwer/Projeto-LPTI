a22d8e6fb2af4e890f7d95bd74bfa6e006653473
Vitor Carvalho de Melo <vitor27melo@gmail.com>
2016-08-18 11:57:06 -0300

    Add files via upload
    Identifica o tipo de usuário

--- site/enter-login.php
+++ site/enter-login.php
@@ -8,10 +8,17 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
   // Detalhe: faz uma verificação com isset() pra saber se o campo foi preenchido
   $email = (isset($_POST['email'])) ? $_POST['email'] : '';
   $senha = (isset($_POST['senha'])) ? $_POST['senha'] : '';
+  $tipo = (isset($_POST)['TipoUsuario_idTipoUsuario'])) ? $_POST['TipoUsuario_idTipoUsuario'] : '';
   // Utiliza uma função criada no seguranca.php pra validar os dados digitados
   if (validaEmail($email, $senha) == true) {
     // O usuário e a senha digitados foram validados, manda pra página interna
-    header("Location:admin/inicio.php");
+    
+    //Verifica se o usuário é administrador ou comum
+    if($tipo == 2){
+		header("Location:admin/inicio.php");
+	}else if($tipo == 1){
+		header("Location:forum-calendario/forum.html");
+	}
   } else {
     // O usuário e/ou a senha são inválidos, manda de volta pro form de login
     // Para alterar o endereço da página de login, verifique o arquivo seguranca.php
