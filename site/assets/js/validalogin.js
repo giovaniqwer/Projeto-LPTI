97f5d92de3de32e01acca57252f3674a06af74f0
Vitor Carvalho de Melo <vitor27melo@gmail.com>
2016-08-18 11:41:48 -0300

    Add files via upload
    Remoção do alert para campos vazios e substituição dos placeholders do input

--- site/assets/js/validalogin.js
+++ site/assets/js/validalogin.js
@@ -1,18 +1,29 @@
-//Rafaela
+//Rafaela e Vitor
 //Javascript para fazer validação do login
 function validaLogin() {
-  var senha = document.querySelector("#inputPassword3").value;
-	//Chamando as verificações
-	valida();
+	if(validaEmBranco()){
+		return true;	
+	}else{
+		return false;	
+	}
 
-	return false;
 }
 //verifica campos em branco
-function valida() {
+function validaEmBranco() {
 	var senhaL = document.querySelector("#inputPassword3").value;
  	var emaiL = document.querySelector("#inputEmail3").value;
-
- 	if((senhaL=="") || (emailL=="")){
-		alert("Existem campos em branco");
- 	}
+	var ver = true;	
+	if(emaiL==""){
+		document.getElementsByName('email')[0].placeholder='Campo em branco';
+		ver = false;	
+	}
+	if(senhaL==""){
+		document.getElementsByName('senha')[0].placeholder='Campo em branco';
+		ver = false;	
+	}
+	if(ver){
+		return true;
+	}else {
+		return false;	
+	}
 }
