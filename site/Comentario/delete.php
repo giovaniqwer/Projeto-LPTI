081cfabaf58f4178cf997a73a177019da3a0145f
Rafaela1301 <rafa.b.custodio13@gmail.com>
2016-09-21 20:28:50 -0300

    demandas

--- site/Comentario/delete.php
+++ site/Comentario/delete.php
@@ -1,24 +1,28 @@
 <?php
 
 require_once '../init.php';
-// pega o ID da URL
-$id = isset($_GET['id']) ? $_GET['id'] : null;
-// valida o ID
-if (empty($id)) {
-    echo " ID não informado";
-    exit;
+
+function teste() {
+    // pega o ID da URL
+    $id = isset($_GET['id']) ? $_GET['id'] : null;
+    // valida o ID
+    if (empty($id)) {
+        echo " ID não informado";
+        exit;
+    }
+    // remove do BD
+    $PDO = db_connect();
+    $sql = "DELETE FROM Comentario WHERE idComentario = :id";
+    $stmt = $PDO->prepare($sql);
+    $stmt->bindParam(':id', $id, PDO::PARAM_INT);
+    if ($stmt->execute()) {
+        echo "<script language='JavaScript'> 
+    window.location='" . $_SERVER['HTTP_REFERER'] . "'; 
+    </script> ";
+    } else {
+        echo " Erro ao excluir";
+        print_r($stmt->errorInfo());
+    }
 }
-// remove do BD
-$PDO = db_connect();
-$sql = "DELETE FROM Comentario WHERE idComentario = :id";
-$stmt = $PDO->prepare($sql);
-$stmt->bindParam(':id', $id, PDO::PARAM_INT);
-if ($stmt->execute()) {
-    echo "<script language='JavaScript'> 
-window.location='".$_SERVER['HTTP_REFERER']."'; 
-</script> ";
-} else {
-    echo " Erro ao excluir";
-    print_r($stmt->errorInfo());
-}
+
 ?>
