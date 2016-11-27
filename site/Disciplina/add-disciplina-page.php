e2e920803d387a8c34da0a71308397fc6091fce2
Vitor Carvalho de Melo <vitor27melo@gmail.com>
2016-11-27 16:34:48 -0200

    Add files via upload

---
+++ site/Disciplina/add-disciplina-page.php
@@ -0,0 +1,52 @@
+<?php /*session_start();
+if (empty($_SESSION["emailID"]) || empty($_SESSION["emailNome"]) || empty($_SESSION["emailTipo"])) {
+    header("Location:../login.php");
+} else if ($_SESSION["emailTipo"] != 1) {
+    header("Location:../negado.html");
+}*/
+?>
+<form action="add-disciplina.php" method="post" enctype="multipart/form-data">
+    <div class="form-group">
+        <label>Nome da Disciplina:</label>
+        <input name="editNomeDisciplina" class="form-control" type="text" required="">
+
+    </div>
+    <div class="form-group">
+        <label>Créditos:</label>
+        <input name="editCredito" class="form-control" type="text" required="">
+    </div>
+    <div class="form-group">
+        <label>Tipo:</label>
+        <select data-toggle="dropdown" id="tipo" name=tipo class="btn btn-primary dropdown-toggle"><span class="caret" required=""></span>
+            <ul class="dropdown-menu">
+                <option value="Comum">Comum</option>
+                <option value="DOB Adm. Pública">DOB Adm. Pública</option>
+                <option value="DOB Economia">DOB Economia</option>
+                <option value="DOB C. Atuariais">DOB C. Atuariais</option>
+                <option value="Optativa C. Atuariais">Optativa C. Atuariais</option>
+                <option value="Optativa Economia">Optativa Economia</option>
+                <option value="Optativa Adm. Pública">Optativa Adm. Pública</option>
+            </ul>
+        </select>
+    </div>
+    <div class="form-group">
+        <label>Periodo:</label>
+        <select data-toggle="dropdown" id="periodo" name=periodo class="btn btn-primary dropdown-toggle"><span class="caret" required=""></span>
+            <ul class="dropdown-menu">
+                <option value="1">1</option>
+                <option value="2">2</option>
+                <option value="3">3</option>
+                <option value="4">4</option>
+                <option value="5">5</option>
+                <option value="6">6</option>
+
+            </ul>
+        </select>
+
+    </div>
+    <div class="form-group">
+      <label>Ementa</label>
+        <input type="file" name="arquivo"/>
+    </div>
+    <button type="submit" id="materia" class="btn btn-info">Salvar</button>
+  </form>
--- site/Disciplina/add-disciplina.php
+++ site/Disciplina/add-disciplina.php
@@ -1,5 +1,39 @@
 <?php
+  session_start();
+  require_once 'conexao.php';
+  require_once 'disciplina-class.php';
 
+  $Tamanho_max = 100000;
+  //Lógica de Inserção
+  $dadosOK = true;
+  echo $_FILES['arquivo']['name'];
+  $arquivo = $_FILES['arquivo'];
+  if($arquivo['error'] != 0){
+    $dadosOK = false;
+    echo "Erro no upload do arquivo! <br>";
+  }else if($arquivo['size'] > $Tamanho_max){
+    echo "Arquivo muito grande! <br>";
+    $dadosOK = false;
+  }else{
+    $file_src = '../uploads/'.$_FILES['arquivo']['name'];
+    if(!move_uploaded_file($_FILES['arquivo']['tmp_name'], $file_src)){
+      echo "Erro ao mover arquivo <br>";
+      $dadosOK = false;
+  }
+  if($dadosOK){
+    $mysqlfile = addslashes(fread(fopen($file_src, "rb"),$arquivo['size']));
+    $disciplina = new Disciplina($_POST['editNomeDisciplina'],$_POST['editCredito'], $_POST['tipo'], $_POST['periodo'], $mysqlfile);
+    $MySQL = new MySQL;
+
+    try{
+      $MySQL->inserirDisciplina($disciplina->getEmenta(),$disciplina->getNome(),$disciplina->getCreditos(),$disciplina->getPeriodo(),$disciplina->getTipo());
+      echo "Dados gravados com sucesso <br>";
+    }catch (Exception $e){
+      echo "Erro ao inserir: ". $e->getMessage() . "<br>";
+    }
+  }*/
+
+/*
 session_start();
 require_once '../init.php';
 include_once 'disciplina-class.php';
@@ -9,15 +43,27 @@ include_once 'disciplina-class.php';
 $nome= isset($_POST['editNomeDisciplina']) ? $_POST['editNomeDisciplina'] : null;
 $creditos= isset($_POST['editCredito']) ? $_POST['editCredito'] : null;
 $periodo = isset($_POST['periodo']) ? $_POST['periodo'] : null;
-$ementa = isset($_FILES['conteudoEmenta']) ? $FILES_['conteudoEmenta'] : null;
+$ementa = isset($_FILES['arquivo']) ? $FILES_['arquivo'] : null;
 $curso = 1;
 $tipo = isset($_POST['tipo']) ? $_POST['tipo'] : null;
 
 $materia = new Disciplina($ementa, $nome, $creditos, $curso, $periodo,$tipo);
 
+
+$uploaddir = '../uploads/';
+$uploadfile = $uploaddir . basename($ementa['name']);
+echo $ementa['name'];
+
+if (move_uploaded_file($_FILES["arquivo"]["name"], $uploaddir)) {
+    echo "Arquivo válido e enviado com sucesso.\n";
+} else {
+    echo "Falha no envio!\n";
+}
+
+
 // insere no BD
 $PDO = db_connect();
-$sql = "INSERT INTO Disciplina(ementa,nome, creditos, Curso_idCurso,periodo,tipo) VALUES(:ementa, :nome, :creditos, :curso, :periodo,:tipo)";
+$sql = "INSERT INTO Disciplina(ementa, nome, creditos, Curso_idCurso,periodo, tipo) VALUES(:ementa, :nome, :creditos, :curso, :periodo, :tipo)";
 $stmt = $PDO->prepare($sql);
 @$stmt->bindParam(':ementa', $materia->getEmenta());
 @$stmt->bindParam(':nome', $materia->getNome());
@@ -33,5 +79,5 @@ if ($stmt->execute()) {
 
     echo "Erro ao cadastrar Disciplina!!";
     print_r($stmt->errorInfo());
-}
+}*/
 ?>
